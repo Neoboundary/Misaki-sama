cd7c43b76b6c89345968f3f26ea633f0fb114fd7
Neoboundary <lugisafrit@gmail.com>
2025-07-01 01:53:01 +0900

    Update start-server.bat

--- start-server.bat
+++ start-server.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Starting Node.js server and ngrok via PowerShell...
-powershell.exe -ExecutionPolicy Bypass -File "%~dp0start_server.ps1"
+powershell.exe -NoExit -ExecutionPolicy Bypass -File "%~dp0start_server.ps1"
 pause
