5f021d78cd1a6cb7ec757e97645e58339515dc7b
Neoboundary <lugisafrit@gmail.com>
2025-07-01 01:17:15 +0900

    Update start-game.bat

--- start-game.bat
+++ start-game.bat
@@ -1,3 +1,5 @@
+setlocal enabledelayedexpansion
+
 rem 1. Node.js on
 echo [1/3] Starting Node.js server...
 start "nodeserver" cmd /c "node server.js"
@@ -5,21 +7,20 @@ timeout /t 2 >nul
 
 rem 2. ngrok on
 echo [2/3] Starting ngrok tunnel on port 3000...
-cd C:\Users\nagar\downloads\ngrok-v3
 start "ngrok" cmd /c "cd /d "C:\Users\nagar\Downloads\ngrok-v3" && .\ngrok http 3000"
 timeout /t 2 >nul
 
 rem 3. retrieve public URL from ngrok webapi
 echo [3/3] Retrieving public URL
-start "public_url" cmd /c "$urldata = curl.exe -s http://127.0.0.1:4040/api/tunnels"
-if ($urldata -match 'https://[^"]*'){
-  $public_url = $matches[0]
-}
+
+for /f "usebackq delims=" %%A in (`powershell -Command "curl.exe -s http://127.0.0.1:4040/api/tunnels | ForEach-Object { if (\$_ -match 'https://[^\""]*') { Write-Output \$matches[0] } }"`) do (
+    set "url=%%A"
+)
 
 rem copy to clipboard
-powershell -NoProfile -Command "Set-Clipboard '$public_url'"
+powershell -NoProfile -Command "Set-Clipboard '!url!'"
 echo Public URL copied to clipboard:
-echo $public_url
+echo !url!
 
 echo.
 echo Press any key to stop services and exit.
